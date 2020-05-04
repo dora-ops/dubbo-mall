24cf8623475eb9b9b98b683dda8b091e9e80b094
chenxuezhou <1134133509@qq.com>
2020-05-04 21:32:42 +0800

    删除idea

---
+++ run-manger.bat
@@ -0,0 +1,2 @@
+title manager
+"C:\Program Files\Java\jdk1.8.0_171\bin\java" -Dmaven.multiModuleProjectDirectory=xmall-master\xmall-manager -Dmaven.home=D:\apache-maven-3.5.3 -Dclassworlds.conf=D:\apache-maven-3.5.3\bin\m2.conf  -Dfile.encoding=UTF-8 -classpath D:\apache-maven-3.5.3\boot\plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -s D:\apache-maven-3.5.3\conf\settings.xml -Dmaven.repo.local=D:\m2 tomcat7:run-war -f xmall-manager\pom.xml
---
+++ run-sso.bat
@@ -0,0 +1,2 @@
+title sso
+"C:\Program Files\Java\jdk1.8.0_171\bin\java" -Dmaven.multiModuleProjectDirectory=xmall-master\xmall-sso -Dmaven.home=D:\apache-maven-3.5.3 -Dclassworlds.conf=D:\apache-maven-3.5.3\bin\m2.conf  -Dfile.encoding=UTF-8 -classpath D:\apache-maven-3.5.3\boot\plexus-classworlds-2.5.2.jar org.codehaus.classworlds.Launcher -s D:\apache-maven-3.5.3\conf\settings.xml -Dmaven.repo.local=D:\m2 tomcat7:run-war -f xmall-sso\pom.xml
